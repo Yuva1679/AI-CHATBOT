df5a7661403c062abcc78800531292760650be0d
Yuvashree C <86184906+Yuva1679@users.noreply.github.com>
2022-02-22 19:01:55 +0530

    Create project.py

---
+++ project.py
@@ -0,0 +1,20 @@
+#question:answer
+import time
+now = time.ctime()
+
+qna = {
+    "hi":"hey",
+    "how are you": "I am fine",
+    "what is your name":"My name is Yuavshree",
+    "how old are you":"I am 20 years old",
+    "what is the time now": now,
+}
+
+while True:
+    qs = input()
+
+    if(qs == "quit"):
+        break
+
+    else:
+        print(qna[qs])
